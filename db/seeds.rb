69b932691ec148c6791aa30af737a7e7eb3d2980
ParishBen <ParishBen13@gmail.com>
2020-08-07 16:24:31 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Recipe.create(name: "Pasta", ingredients: "flour, starch, dough, yeast, eggs, water", cook_time: "2.5 hours")
+Recipe.create(name: "PBJ", ingredients: "PB&J, Bread", cook_time: "3minutes")
+
+
+Recipe.create(name: "chicken sandwich", ingredients: "chicken breast, spices, lettuce, pickles, bbq sauce, cheese", cook_time: "50minutes")
+
+Recipe.create(name: "eggs & bacon", ingredients: "eggs, bacon, bread", cook_time: "30 minutes")
