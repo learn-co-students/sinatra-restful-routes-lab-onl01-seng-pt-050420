03dd1d5745f623d9a6b3acda90e8f25e1ae54a43
danielkwon89 <danielkwon89@gmail.com>
2020-08-11 15:58:16 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,6 @@ class ApplicationController < Sinatra::Base
     @recipe.ingredients = params["ingredients"]
     @recipe.cook_time = params["cook_time"]
     @recipe.save
-    # binding.pry
     redirect to "/recipes/#{@recipe.id}"
   end
 
