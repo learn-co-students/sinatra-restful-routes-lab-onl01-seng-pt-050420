946ac9b46e618373151b898a5ac8b1bcc07624a0
ryancdavey1 <ryancdavey1@gmail.com>
2020-08-13 00:12:45 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,22 +10,22 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
-  get '/recipes' do
+  get '/recipes' do #loads index page
     @recipes = Recipe.all
     erb :index
   end
 
-  get '/recipes/:id' do
+  get '/recipes/:id' do  #loads show page
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
 
-  get '/recipes/:id/edit' do
+  get '/recipes/:id/edit' do #loads edit form
     @recipe = Recipe.find_by_id(params[:id])
-    erb :show
+    erb :edit
   end
 
-  patch '/recipes/:id' do
+  patch '/recipes/:id' do  #updates a recipe
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
@@ -34,15 +34,15 @@ class ApplicationController < Sinatra::Base
     redirect to "/recipes/#{@recipe.id}"
   end
 
-  post '/recipes' do 
+  post '/recipes' do  #creates a recipe
     @recipe = Recipe.create(params)
     redirect to "/recipes/#{@recipe.id}"
   end
 
-  delete '/recipes/:id' do 
+  delete '/recipes/:id' do #destroy action
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.delete
     redirect to '/recipes'
   end
-  
+
 end
